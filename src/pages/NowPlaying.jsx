8eb09ad7e2f8cf85f854c53f0de0280175010abf
Hellowwwcodingsdf <ammiller1151@gmail.com>
2025-05-17 12:29:49 -0400

    Update NowPlaying.jsx

--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,9 +1,13 @@
+import React from "react";
 import { Reel } from "../components";
 
 const NowPlaying = (props) => {
     return (
-        <Reel tmdb={props.tmdb} uri="now_playing" title="Now Streaming" /> 
+        <div className="h-screen w-screen bg-[url('/background.png')] bg-cover bg-center text-shadow fade-bottom flex items-center justify-center">
+            <Reel tmdb={props.tmdb} uri="now_playing" title="Now Streaming" />
+        </div>
     );
-}
+};
 
 export default NowPlaying;
+
